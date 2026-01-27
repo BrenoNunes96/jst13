cdc30c36e3d5948b517b8c1955f10570953a120e
BrenoNunes96 <brenocp3@live.com>
2026-01-27 11:46:32 -0300

    variaveis

---
+++ aula2/Variaveis.js
@@ -0,0 +1,22 @@
+
+const leia = require('readline-sync')
+
+
+let salario = 20000
+let bonus = 200
+
+console.log(new Intl.NumberFormat("pt-br",{
+  style:"currency",
+  currency:'BRl',
+
+}).format(salario+(bonus + salario)))
+
+let num2 =42
+let num = '42'
+
+
+if(num !== num2){
+
+console.log('diferente tipo e valor')
+
+}
